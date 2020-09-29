6581dd8b25eecdea66609ec0605cb931d37f0fc9
Sirbelog <sirbelog@mail.ru>
2020-09-29 13:37:21 +0900

    localstorage

--- todoList/script.js
+++ todoList/script.js
@@ -4,51 +4,129 @@ let input = document.querySelector(".input")
 let button = document.querySelector(".button")
 let list = document.querySelector(".list")
 let clear = document.querySelector(".button-clear")
+let deleteModal = document.querySelector(".delete__modal")
+
+
+let itemsArray = [];
+let saving = JSON.parse(localStorage.getItem("data"))
+
+
+
+
+
+	// Проверка на наличие сохраненных элементов 
+
+if(localStorage.getItem('data')!=undefined){
+	itemsArray = saving
+	for (let i = 0; i<saving.length;i++){
+		
+	let addlist = document.createElement('li')
+	addlist.innerHTML = saving[i]
+	list.appendChild(addlist)	
+
+	dellists(addlist);
+	donelists(addlist);
+
+	}
+
+	
+}else{
+	itemsArray = [];
+	localStorage.clear();
+}
+
+
+
+	//  Функция добавления нового элемента
 
 function addElem(){
+
 	let addlist = document.createElement('li')
 	addlist.innerHTML = input.value
 	list.appendChild(addlist)
 
+	dellists(addlist);
+	donelists(addlist);
+	
+}
+
+
+	// Функция удаления каждого элемента 
+
+function dellists(value,index) {
+
 	let dellist = document.createElement("div")
 	dellist.className = "dellist"
-	addlist.appendChild(dellist)
+	value.appendChild(dellist)
 	dellist.innerHTML ="&#10006;"
+
+
+	dellist.addEventListener("click", function(){
+		list.removeChild(value)
+		
+	})	
+}
+
+	// Функция для отметки выполненных дел
+
+function donelists(value) {
 
 	let donelist = document.createElement("div")
 	donelist.className = "donelist"
-	addlist.appendChild(donelist)
+	value.appendChild(donelist)
 	donelist.innerHTML ="&#10004;"
-	
 
-	dellist.addEventListener("click", function(){
-		list.removeChild(addlist)
-	})
+	
 
 	donelist.addEventListener("click", function(){
 		
-			addlist.style.backgroundColor = "#98FB98";
-			addlist.removeChild(donelist)
-
-	})
-
-	clear.addEventListener ("click",function(){
-		list.removeChild(addlist)
-		button.classList.add("jump")
+		value.style.backgroundColor = "#98FB98";
+		value.removeChild(donelist)
 	})
 }
 
 
 
+	// Кнопка добавления элементов
+
 button.addEventListener("click", function(){
+	
 	if(input.value != ""){
+
 		addElem()
+
+		itemsArray.push(input.value)
+		localStorage.setItem("data", JSON.stringify(itemsArray))
+
 		button.classList.remove("jump")
 		input.value = ""
 	}else{
-		input.value = " "
+		input.value = ""
 	}
+	
 })
 
 
+	// Кнопка полной очистки 
+
+clear.addEventListener ("click",function(){
+
+	let buttonYes = document.querySelector(".button__yes")
+	let buttonNo = document.querySelector(".button__no")
+
+	deleteModal.classList.add('delete__modal-block')
 	
+	buttonYes.addEventListener('click',function(){
+		list.innerHTML = ""
+		button.classList.add("jump")
+		localStorage.clear();
+		itemsArray = [];
+		deleteModal.classList.remove('delete__modal-block')
+	})
+
+	buttonNo.addEventListener('click',function(){
+		deleteModal.classList.remove('delete__modal-block')
+	})
+	
+		
+})
